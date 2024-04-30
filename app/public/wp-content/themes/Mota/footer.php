379b662ef8cbf1f30b759a8449dafcf91270673d
Caro1210 <c.delannoy12@gmx.com>
2024-04-30 15:15:36 +0200

    header & footer

---
+++ app/public/wp-content/themes/Mota/footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <nav>     
+        <?php
+        wp_nav_menu([
+            'theme_location' => 'footer-second-menu',
+        ]);
+        ?>
+        </nav>
+</footer>
---
+++ app/public/wp-content/themes/Mota/header.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <?php wp_head(); ?>
+    </head>
+
+    <body>
+
+    <header>
+        <nav> 
+            <div class="logo_site">
+            <a href="<?php echo get_site_url()?>">
+                <img src="<?php echo get_template_directory_uri() . '/images/logo.png'; ?>" alt="logo site Nathalie Mota">
+            </a>
+            </div>
+        
+        <div class="nav_menu">
+        <?php
+        wp_nav_menu([
+            'theme_location' => 'header-main-menu',
+        ]);
+        ?>
+        </div>
+        </nav>
+    </header>
