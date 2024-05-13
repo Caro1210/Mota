4153f7b56f648e1210ae7c7dc761c2b466a63f3d
Caro1210 <c.delannoy12@gmx.com>
2024-05-13 23:42:27 +0200

    création single-photo

---
+++ app/public/wp-content/themes/Mota/single-photo.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @package Mota
+ * @since 1.0.0
+ */
+
+ get_header(); ?>
+
+ <main id="primary" class="site-main single">
+   <?php if (have_posts()) : ?>
+	 <?php while (have_posts()) : the_post(); ?>
+	   <div class="post">
+		 <h1 class="post-title"><?php the_title(); ?></h1>
+		 <p class="post-info">
+		   Publié le <?php the_date(); ?> dans <?php the_category(', '); ?> par <?php the_author(); ?>.
+		 </p>
+		 <div class="post-content">
+		   <?php the_content(); ?>
+		 </div>
+	   </div>
+	 <?php endwhile; ?>
+   <?php endif; ?>
+ </main><!-- #main -->
+ 
+ <?php get_footer(); ?>
